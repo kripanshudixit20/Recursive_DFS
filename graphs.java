30216a64c814e765e1f86853947017d4a6cd97b6
Kripanshu Dixit <66563984+kripanshudixit20@users.noreply.github.com>
2022-06-12 18:22:37 +0530

    Create graphs.java

---
+++ graphs.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+public class graphs
+{
+    private void deathFirstSearchRecursive(String node, HashMap<String, String[]> neighbors) {
+        System.out.println(node);
+        for (String n : neighbors.get(node))
+        {
+            deathFirstSearchRecursive(n, neighbors);
+        }
+    }
+
+    public static void main(String...args) {
+        graphs ob = new graphs();
+        HashMap<String, String[]> graph = new HashMap<>();
+
+        graph.put("a", new String[]{"b", "c"});
+        graph.put("b", new String[]{"d"});
+        graph.put("c", new String[]{"e"});
+        graph.put("d", new String[]{"f"});
+        graph.put("e", new String[]{});
+        graph.put("f", new String[]{});
+        ob.deathFirstSearchRecursive("a", graph);
+    }
+}
