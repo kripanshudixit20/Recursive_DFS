b13de6248626573d555dee126d8fd5d46d806841
Kripanshu Dixit <66563984+kripanshudixit20@users.noreply.github.com>
2022-06-16 10:35:03 +0530

    Update graphs.java
    Added code to handle cyclic graph condition leading to infinite loop

--- graphs.java
+++ graphs.java
@@ -1,24 +1,31 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 public class graphs
 {
-    private void depthFirstSearchRecursive(String node, HashMap<String, String[]> neighbors) {
-        System.out.println(node);
-        for (String n : neighbors.get(node))
+    private void depthFirstSearchRecursive(String node, HashMap<String, String[]> neighbors, ArrayList<String> visited)
+    {
+        if (!visited.contains(node))
         {
-            deathFirstSearchRecursive(n, neighbors);
+            visited.add(node);
+            System.out.println(node);
+            for (String n : neighbors.get(node))
+            {
+                depthFirstSearchRecursive(n, neighbors, visited);
+            }
         }
     }
 
     public static void main(String...args) {
         graphs ob = new graphs();
         HashMap<String, String[]> graph = new HashMap<>();
+        ArrayList<String> visited = new ArrayList<>();
 
         graph.put("a", new String[]{"b", "c"});
         graph.put("b", new String[]{"d"});
         graph.put("c", new String[]{"e"});
         graph.put("d", new String[]{"f"});
         graph.put("e", new String[]{});
-        graph.put("f", new String[]{});
-        ob.depthFirstSearchRecursive("a", graph);
+        graph.put("f", new String[]{"b"});
+        ob.depthFirstSearchRecursive("a", graph, visited);
     }
 }
